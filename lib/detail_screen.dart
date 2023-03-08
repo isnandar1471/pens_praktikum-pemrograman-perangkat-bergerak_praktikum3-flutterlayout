3846830e05736e7a0e00de9b02bc0b45859523d3
isnandar1471 <isnandar.1471@gmail.com>
2023-03-08 09:27:09 +0700

    percobaan4: tampilan 4 (image)

---
+++ lib/detail_screen.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SafeArea(
+      child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Image.asset('assets/images/submarine.jfif'),
+            Container(
+                margin: EdgeInsets.only(top: 16.0),
+                child: Text(
+                  'Surabaya Submarine Monument',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )),
+            Container(
+                margin: const EdgeInsets.symmetric(vertical: 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Column(
+                      children: const <Widget>[
+                        Icon(Icons.calendar_today),
+                        Text('Open Everyday'),
+                      ],
+                    ),
+                  ],
+                )),
+            Container(
+                padding: const EdgeInsets.all(16.0),
+                child: const Text(
+                  'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(fontSize: 16.0),
+                )),
+            Container(
+              height: 150,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.network(
+                        'https://media-cdn.tripadvisor.com/media/photo-m/1280/16/a9/33/43/liburan-di-farmhouse.jpg'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.asset('assets/images/submarine.jfif'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.asset('assets/images/submarine.jfif'),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(4.0),
+                    child: Image.asset('assets/images/submarine.jfif'),
+                  ),
+                ],
+              ),
+            ),
+          ]),
+    ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:praktikum3_flutterlayout_part1/detail_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,50 +15,5 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(),
       home: const DetailScreen(),
     );
-  }
-}
-
-class DetailScreen extends StatelessWidget {
-  const DetailScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        body: SafeArea(
-      child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Container(
-                margin: EdgeInsets.only(top: 16.0),
-                child: Text(
-                  'Surabaya Submarine Monument',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 30.0,
-                    fontWeight: FontWeight.bold,
-                  ),
-                )),
-            Container(
-                margin: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    Column(
-                      children: const <Widget>[
-                        Icon(Icons.calendar_today),
-                        Text('Open Everyday'),
-                      ],
-                    ),
-                  ],
-                )),
-            Container(
-                padding: const EdgeInsets.all(16.0),
-                child: const Text(
-                  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, omnis quo officiis sed laudantium officia natus commodi pariatur fugiat eum quas dolor consequuntur expedita voluptatem sit rerum voluptates sint perferendis!',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 16.0),
-                ))
-          ]),
-    ));
   }
 }
